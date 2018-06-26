22aca60c0b84c8ed17c32f3a4d2d1f9c8a132c1f
eamuyoung <1782635433@qq.com>
2018-06-26 14:32:05 +0800

    项目建立

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import persistorstore from './store/counter';
+import {Provider} from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+
+const {persistor,store} = persistorstore()
+ReactDOM.render(
+    <Provider store={store}>
+        <PersistGate persistor={persistor}>
+        <App/>
+        </PersistGate>
+    </Provider>
+    ,document.getElementById('app'));

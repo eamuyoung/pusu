36ed5284b36a6a1d668ce6bf6c674bed234342a3
mc-feng <73458955@qq.com>
2018-06-28 15:14:05 +0800

    别墅列表完成

---
+++ src/components/BieshuList/index.js
@@ -0,0 +1,42 @@
+import React,{Component} from 'react';
+import "./BeishuList.scss";
+export default class BeishuList extends Component{
+    render(){
+        var beiContent =[]
+        for( let i=0; i<3;i++){
+            beiContent.push(
+                <div className="col-md-4">
+                <div className="bieshu-image">
+                <img src="./src/public/bieshuimg/bieshu.jpg" alt=""/>
+                </div>
+                <p className="bieshu-title">傍水绿墅-10米挑空度假休闲吧</p>
+                <p className="bieshu-describe">四居室&nbsp;380平&nbsp;宜居10人&nbsp;北京|昌平区</p>
+                <p className="bieshu-price">￥3280</p>
+              </div>
+            )
+        }
+        return(
+            <div className="container text-center">
+               <h1 className="bieshu-heading"> 最受欢迎的别墅</h1>
+               <ul className="bieshu-nav clearfix">
+                   <li><a>北京</a> <span className="bieshu-nav-bottom"></span></li>
+                   <li><a>杭州</a><span className="bieshu-nav-bottom"></span></li>
+                   <li><a>上海</a><span className="bieshu-nav-bottom"></span></li>
+                   <li><a>深圳</a><span className="bieshu-nav-bottom"></span></li>
+                   <li><a>天津</a><span className="bieshu-nav-bottom"></span></li>
+                   <li><a>成都</a><span className="bieshu-nav-bottom"></span></li>
+                   <li><a>大理</a><span className="bieshu-nav-bottom"></span></li>
+                   <li><a>三亚</a><span className="bieshu-nav-bottom"></span></li>
+                   <li><a>丽江</a><span className="bieshu-nav-bottom"></span></li>
+               </ul>
+               <div className="bieshu-content row">
+                    {beiContent}
+               </div>
+               <div className="bieshu-content row">
+                    {beiContent}
+               </div>
+               <h1 className="bieshu-heading"> 住墅路上的故事</h1>
+            </div>
+        )
+    }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export Headernav from "./Headernav";
 export Carousel from "./Carousel";
 export Bieshu from "./Bieshu";
 export SearchBox from "./SearchBox";
+export BeishuList from "./BieshuList"
